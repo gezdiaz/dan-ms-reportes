5274e9bddfac79d77995d33af80b5b2b1828ad13
Gaston <gaston.diaz.contractor@macmillan.com>
2021-08-11 21:57:37 -0300

    spring initializr.

---
+++ src/main/java/dan/tp2021/reportes/ReportesApplication.java
@@ -0,0 +1,13 @@
+package dan.tp2021.reportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dan/tp2021/reportes/ReportesApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.tp2021.reportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
