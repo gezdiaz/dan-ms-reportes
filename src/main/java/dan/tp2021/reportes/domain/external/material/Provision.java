d452cd42b83d40f79682396b1116ef94e5fc2246
Gaston <gaston.diaz.contractor@macmillan.com>
2021-09-05 20:11:28 -0300

    correcion en la provision

--- src/main/java/dan/tp2021/reportes/domain/external/material/Provision.java
+++ src/main/java/dan/tp2021/reportes/domain/external/material/Provision.java
@@ -1,11 +1,15 @@
 package dan.tp2021.reportes.domain.external.material;
 
+import java.time.Instant;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import dan.tp2021.reportes.dao.utils.SoloLectura;
@@ -17,9 +21,7 @@ public class Provision {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @ManyToOne(optional = false)
-    private Material material;
-    private Integer cantidad;
-    @ManyToOne
-    private Provision provision;
+    private Instant fechaProvision;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<DetalleProvision> detalle;
 }
